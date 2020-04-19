f850fe93c25bc8b37f1612c48386d888fc2ea9d5
Abu-Al Mahadi <mohammed.abumahadi@northsouth.edu>
2020-04-19 16:50:20 +0600

    Dummy Question

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./assets/style.css";
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
 
 class ZendQuiz extends React.Component {
+  state = {
+    questionBank: [],
+  };
+
+  getQuestions = () => {
+    quizService().then((question) =>
+      this.setState({
+        questionBank: question,
+      })
+    );
+  };
+
+  componentDidMount() {
+    this.getQuestions();
+  }
+
   render() {
     return (
       <div className="container">
         <div className="title">Zend Preparation</div>
+        {this.state.questionBank.length > 0 &&
+          this.state.questionBank.map(
+            ({ question, answers, correct, questionId }) => (
+              <QuestionBox
+                question={question}
+                options={answers}
+                key={questionId}
+              />
+            )
+          )}
       </div>
     );
   }
